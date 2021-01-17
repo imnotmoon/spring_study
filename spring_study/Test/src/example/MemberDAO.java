6e90c2453ab982e9f4a5184121deafa18200c1c2
imnotmoon <imnotmoon@gmail.com>
2021-01-18 06:07:21 +0900

    p236. Access DB via JDBC Connection

---
+++ spring_study/Test/src/example/MemberDAO.java
@@ -0,0 +1,62 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MemberDAO {
+    private Statement stmt;
+    private Connection con;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/DatabaseServlet?serverTimezone=UTC";
+    private String user = "root";
+    private String password = "maupas";
+
+    public List<MemberVO> listMembers() {
+        List<MemberVO> list = new ArrayList<MemberVO>();
+        try {
+            connDB();
+            String query = "select * from t_member";
+            System.out.println(query);
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()) {
+                String id = rs.getString("id");
+                String pwd = rs.getString("pwd");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date joinDate = rs.getDate("joinDate");
+
+                MemberVO vo = new MemberVO();
+                vo.setId(id);
+                vo.setPwd(pwd);
+                vo.setName(name);
+                vo.setEmail(email);
+                vo.setJoinDate(joinDate);
+                list.add(vo);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void connDB() {
+        try {
+            Class.forName(driver);
+            System.out.println("Oracle driver loading succeed");
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection genererated Succeed");
+            stmt = con.createStatement();
+            System.out.println("Statement generated Succeed");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ spring_study/Test/src/example/MemberServlet.java
@@ -0,0 +1,41 @@
+package example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+@WebServlet(name = "MemberServlet", value = "/member")
+public class MemberServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        MemberDAO dao = new MemberDAO();
+        List<MemberVO> list = dao.listMembers();
+
+        out.print("<html><body>");
+        out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+        out.print("<td>ID</td> <td>PW</td> <td>NAME</td> <td>EMAIL</td> <td>JOINDATE</td> </tr>");
+
+        for(int i=0; i< list.size(); i++) {
+            MemberVO memberVO = list.get(i);
+            String id = memberVO.getId();
+            String pwd = memberVO.getPwd();
+            String name = memberVO.getName();
+            String email = memberVO.getEmail();
+            Date joinDate = memberVO.getJoinDate();
+
+            out.print("<tr><td>" + id + "</td><td>" + pwd + "</td><td>" + name + "</td><td>" + email + "</td><td>" + joinDate + "</td><td><tr>");
+        }
+        out.print("</table></body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
