8809e7e786ca6b89c5d4ac9a885dd9814f888c3d
imnotmoon <imnotmoon@gmail.com>
2021-01-18 14:51:34 +0900

    DB Access with Connection Pool

---
+++ DatabaseAccess/src/ex01/MemberDAO.java
@@ -0,0 +1,62 @@
+package ex01;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MemberDAO {
+    private Statement stmt;
+    private PreparedStatement pstmt;
+    private Connection con;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/DatabaseServlet?serverTimezone=UTC";
+    private String user = "root";
+    private String password = "maupas";
+
+    public List<ex01.MemberVO> listMembers() {
+        List<ex01.MemberVO> list = new ArrayList<ex01.MemberVO>();
+        try {
+            connDB();
+            String query = "select * from t_member";
+            System.out.println("prepared statement : " + query);
+            pstmt = con.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                String id = rs.getString("id");
+                String pwd = rs.getString("pwd");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date joinDate = rs.getDate("joinDate");
+
+                ex01.MemberVO vo = new ex01.MemberVO();
+                vo.setId(id);
+                vo.setPwd(pwd);
+                vo.setName(name);
+                vo.setEmail(email);
+                vo.setJoinDate(joinDate);
+                list.add(vo);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void connDB() {
+        try {
+            Class.forName(driver);
+            System.out.println("Oracle driver loading succeed");
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection genererated Succeed");
+            stmt = con.createStatement();
+            System.out.println("Statement generated Succeed");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
