030e957bc880703d31858844198d94b5c7559239
imnotmoon <imnotmoon@gmail.com>
2021-02-14 00:47:10 +0900

    ch19 : xml을 통한 DI

---
+++ chapter19/src/test/java/com/spring/chapter19/Chapter19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.chapter19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
